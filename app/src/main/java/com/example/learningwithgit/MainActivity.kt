f9089989321797669d74249fed6ea91d67f7ed0f
nloktionov <nol3000>
2022-03-30 20:24:49 +0300

    Store data. Preferences. Done.

--- app/src/main/java/com/example/learningwithgit/MainActivity.kt
+++ app/src/main/java/com/example/learningwithgit/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.example.learningwithgit
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.learningwithgit.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var sPref: SharedPreferences
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonLoad.setOnClickListener(this)
+        binding.buttonSave.setOnClickListener(this)
+
+        loadText()
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            binding.buttonLoad.id -> {
+                loadText()
+            }
+            binding.buttonSave.id -> {
+                saveText()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        saveText()
+    }
+
+
+
+    private fun saveText() {
+        sPref = getPreferences(MODE_PRIVATE)
+        val ed = sPref.edit()
+        ed.putString("saved_text", binding.EditText.text.toString())
+        ed.apply()
+        Toast.makeText(this, "text saved!", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun loadText() {
+        sPref = getPreferences(MODE_PRIVATE)
+        var savedText = sPref.getString("saved_text", "")
+        binding.EditText.setText(savedText)
+        Toast.makeText(this, "text loaded!", Toast.LENGTH_SHORT).show()
     }
 }
